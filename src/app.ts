3797586001a6d90c188bbc1e88a57bdbe961c6eb
Rizka Rahmadina <hi.rizkarahmadina@gmail.com>
2026-02-17 02:45:17 +0700

    fix: resolve cors error for vercel deployment

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,7 @@ import path from "path";
 
 const app = express();
 
-app.use(cors({
-  origin: ["http://localhost:3000", "https://ecommerce-sport-web.vercel.app/"], 
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({limit: "10mb", extended: true}));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
